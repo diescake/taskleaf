7f7438efa4cbababdb56f58753f42091b096ee65
diescake <daisuke.kondo@smarthr.co.jp>
2021-01-20 05:06:41 +0900

    ✨ Implement CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,29 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
