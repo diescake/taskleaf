40852cde38f45e6b720a1dfb5d24906a7e6c4231
diescake <daisuke.kondo@smarthr.co.jp>
2021-01-22 01:48:24 +0900

    🔧 編集画面の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.order(:id)
   end
 
   def show
@@ -12,6 +12,13 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def create
