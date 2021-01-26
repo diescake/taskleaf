91b4bc3b0a9d845c0e1497d1e07ea223fb4ba294
diescake <daisuke.kondo@smarthr.co.jp>
2021-01-27 03:16:21 +0900

    🔧 Implement login feature

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: session_params[:email])
+
+    if @user&.authenticate(session_params[:password])
+      puts session
+      puts session[:user_id]
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
