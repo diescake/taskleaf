0ce99d91a68449ac592e69d1febc757928a24da8
diescake <daisuke.kondo@smarthr.co.jp>
2021-01-30 17:24:10 +0900

    ✅ Add spec for tasks#show

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,28 +1,49 @@
 require 'rails_helper'
 
 describe 'タスク管理機能' do
-  describe '一覧表示機能' do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let(:user_admin) { FactoryBot.create(:user, name: '管理者ユーザー', email: 'admin@example.com', admin: true) }
+  let(:user_guest) { FactoryBot.create(:user, name: 'ゲストユーザー', email: 'guest@example.com', admin: false) }
 
-    before do
-      FactoryBot.create(:task, name: 'タスクA', user: user_a)
-      FactoryBot.create(:task, name: 'タスクB', user: user_a)
-      FactoryBot.create(:task, name: 'タスクC', user: user_a)
+  let!(:task_a1) { FactoryBot.create(:task, name: 'タスクA-1', user: user_a)}
+  let!(:task_a2) { FactoryBot.create(:task, name: 'タスクA-2', user: user_a)}
+  let!(:task_a3) { FactoryBot.create(:task, name: 'タスクA-3', user: user_a)}
+  let!(:task_b1) { FactoryBot.create(:task, name: 'タスクB-1', user: user_b)}
 
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  describe 'ナビゲーション機能' do
+    context '管理者権限のあるユーザーがログインしている時' do
+      let(:login_user) { user_admin }
+
+      it 'ユーザー一覧が表示されること' do
+        expect(page).to have_content 'ユーザー一覧'
+      end
     end
 
+    context '管理者権限がないユーザーがログインしている時' do
+      let(:login_user) { user_guest }
+
+      it 'ユーザー一覧が表示されないこと' do
+        expect(page).to have_no_content 'ユーザー一覧'
+      end
+    end
+  end
+
+  describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
       it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content 'タスクA'
-        expect(page).to have_content 'タスクB'
-        expect(page).to have_content 'タスクC'
+        expect(page).to have_content 'タスクA-1'
+        expect(page).to have_content 'タスクA-2'
+        expect(page).to have_content 'タスクA-3'
       end
     end
 
@@ -30,9 +51,29 @@ describe 'タスク管理機能' do
       let(:login_user) { user_b }
 
       it 'ユーザーAが作成したタスクが表示されない' do
-        expect(page).to have_no_content 'タスクA'
-        expect(page).to have_no_content 'タスクB'
-        expect(page).to have_no_content 'タスクC'
+        expect(page).to have_no_content 'タスクA-1'
+        expect(page).to have_no_content 'タスクA-2'
+        expect(page).to have_no_content 'タスクA-3'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      it 'ユーザーAのタスクが表示される' do
+        visit task_path(task_a1)
+        expect(page).to have_content 'タスクA-1'
+      end
+    end
+
+    context 'ユーザーBがログインしている時' do
+      let(:login_user) { user_b }
+
+      it 'ユーザーBのタスクが表示される' do
+        visit task_path(task_b1)
+        expect(page).to have_content 'タスクB-1'
       end
     end
   end
