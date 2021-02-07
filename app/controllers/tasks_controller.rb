28b38317ca0b986a1cf3024463941cef8e584ab4
diescake <daisuke.kondo@smarthr.co.jp>
2021-02-07 14:32:38 +0900

    🔧 Import tasks through csv

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,6 +52,11 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: 'タスクを追加しました。'
+  end
+
   private
 
   # Only allow a list of trusted parameters through.
