b7307279c8b29f8d1e66d8ac3ec7eba2805979ee
diescake <daisuke.kondo@smarthr.co.jp>
2021-02-11 23:06:08 +0900

    🔧 Hello JSX

--- app/javascript/taskleaf/hello.js
+++
@@ -1,9 +0,0 @@
-import React, { createElement } from 'react'
-import ReactDOM from 'react-dom'
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    createElement('div', null, 'Hello world!!'),
-    document.getElementById('app'),
-  )
-})
